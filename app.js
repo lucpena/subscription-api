17967f1dbadf36bdfc796d850c4c56e221405f0b
Lucas Pena <lucaspena4@hotmail.com>
2026-04-06 21:25:10 -0300

    Database, Middleware, Models and Requests

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import { PORT } from "./config/env.js";
+
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+// Default Express middleware to parse JSON
+app.use(express.json());
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+// Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+// Error handling middleware (should be last)
+app.use(errorMiddleware);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!'); 
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
---
+++ models/subscription.model.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [ true, 'Subscription Name is required' ],
+        trim: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    price: {
+        type: Number,
+        required: [ true, 'Price is required' ],
+        min: [0, 'Price must be a positive number']
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'BRL'],
+        default: 'USD'
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly']
+    },
+    category: {
+        type: String,
+        enum: ['entertainment', 'utilities', 'subscriptions', 'sports', 'education', 'health', 'travel', 'food', 'shopping', 'other'],
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive', 'expired'],
+        default: 'active'
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return value <= new Date();
+            },
+            message: 'Start date cannot be in the future'
+        }
+    },
+    renewalDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return value >= this.startDate;
+            },
+            message: 'Renewal date must be after start date'
+        }
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    }            
+}, { timestamps: true });
+
+// Auto calculate renewal date if missing
+subscriptionSchema.pre('save', function(next) {
+    if ( !this.renewalDate ) {
+        const renewalPeriods = {
+            daily: 1,
+            weekly: 7,
+            monthly: 30,
+            yearly: 365
+        };
+        
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
+    }
+
+    // Auto update the status if renewal date has passed
+    if ( this.renewalDate < new Date() ) {
+        this.status = 'expired';
+    }
+
+    next();
+});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+export default Subscription;
