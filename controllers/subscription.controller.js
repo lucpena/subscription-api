589f46e4d7f5ca80c8db835a75203d7c711a8487
Lucas Pena <lucaspena4@hotmail.com>
2026-04-08 22:03:27 -0300

    Nodemailer, Upstash, Reminder e-mails, E-mails templates and fixes

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,8 @@
 import Subscription from '../models/subscription.model.js';
 
+import { workflowClient } from '../config/upstash.js';
+import { SERVER_URL } from '../config/env.js';
+
 export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create({
@@ -7,7 +10,18 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id
         });
 
-        res.status(201).json({ success: true, data: subscription });
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: { 
+                subscriptionId: subscription.id
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0
+        });
+
+        res.status(201).json({ success: true, data: subscription, workflowRunId });
     } catch (error) {
         next(error);
     }
